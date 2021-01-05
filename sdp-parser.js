64f39b16257230654a0e6c3cc4fc5b021830d9df
Johnny <hellojinqiang@gmail.com>
2021-01-05 15:36:51 +0800

    feat: update sdp config.

--- sdp-parser.js
+++ sdp-parser.js
@@ -10,10 +10,14 @@ fs.readFile('/Users/johnny/Downloads/sdp.json', 'utf8', (err, data) => {
     if (err) {
         return console.log(err)
     }
-    let sdpObject = JSON.parse(data)
-    console.log(sdpObject.offer.origin.sdp)
-    console.log(sdpObject.offer.modified.sdp)
-    console.log(sdpObject.answer.origin.sdp)
-    console.log(sdpObject.answer.modified.sdp)
-    // return sdpObject
+    let obj = JSON.parse(data)
+    // console.log(obj.offer.origin.sdp)
+    // console.log(obj.offer.modified.sdp)
+    // console.log(obj.answer.origin.sdp)
+    // console.log(obj.answer.modified.sdp)
+
+    console.log(obj.offer.origin.text)
+    console.log(obj.offer.modified.text)
+    console.log(obj.answer.origin.text)
+    console.log(obj.answer.modified.text)
 })
